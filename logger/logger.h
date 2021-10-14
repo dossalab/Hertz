a69192f08f92f105d1b12fd350284e388430136e
Roman Luitko <dossalab@pm.me>
2021-10-14 23:40:41 +0200

    add line numbers to error logs

--- logger/logger.h
+++ logger/logger.h
@@ -11,10 +11,15 @@ enum loglevel {
 	LOGLEVEL_SILENT, /* Should be the last (highest) one */
 };
 
+#define logger_expand(x)	#x
+#define logger_line_to_str(x)	logger_expand(x)
+#define logger_file_and_line	__FILE__":"logger_line_to_str(__LINE__)
+
 #define log_d(...) logger_log("DEBUG", NULL, LOGLEVEL_DEBUG, __VA_ARGS__)
 #define log_i(...) logger_log("INFO", NULL, LOGLEVEL_INFO, __VA_ARGS__)
 #define log_w(...) logger_log("WARN", NULL, LOGLEVEL_WARN, __VA_ARGS__)
-#define log_e(...) logger_log("ERROR", __FILE__, LOGLEVEL_ERROR, __VA_ARGS__)
+#define log_e(...) logger_log("ERROR", logger_file_and_line, \
+		LOGLEVEL_ERROR, __VA_ARGS__)
 
 void logger_log(const char *label, const char *meta, enum loglevel level,
 		char *fmt, ...);
