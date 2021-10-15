943b4e0b409810fce497f9830ea19b9f99eb9211
Roman Luitko <dossalab@pm.me>
2021-10-15 11:04:43 +0200

    *actually* use mipmaps

--- gl/textures.c
+++ gl/textures.c
@@ -9,13 +9,13 @@ GLuint create_texture_from_rgb(uint8_t *texture_data, size_t w, size_t h)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
 			w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_data);
 
 	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	return texture;
 }
