0738e74bcd88ec611231a60eb4b002433d9b92d2
Roman Luitko <dossalab@pm.me>
2022-04-22 10:49:04 +0200

    multiple lights support

--- include/hz/built-in/shaders/simple.h
+++ include/hz/built-in/shaders/simple.h
@@ -1,6 +1,8 @@
 #ifndef HZ_BUILTIN_SHADERS_SIMPLE_H
 #define HZ_BUILTIN_SHADERS_SIMPLE_H
 
+#define HZ_SIMPLE_SHADER_NUM_LIGHTS	10
+
 extern const char *hz_simple_fragment_shader_source;
 extern const char *hz_simple_vertex_shader_source;
 
